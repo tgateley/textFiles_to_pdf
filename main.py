90f245e70b718ad3821c5bf6fc779ba4b8ac2cbe
Tiffany <tgateley22@gmail.com>
2024-08-14 23:18:35 -0500

    add content to the pages

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ filepaths = glob.glob("Text_Files/*.txt")
 pdf = FPDF(orientation='P', unit="mm", format="A4")
 for filepath in filepaths:
     pdf.add_page()
+
     with open(filepath) as file:
         content = file.read()
+
     filename = Path(filepath).stem
     name = filename.title()
 
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=0, h=8, txt=name, ln=1)
+
+    pdf.set_font(family="Times", size=12, style="B")
+    pdf.multi_cell(w=0, h=6, txt=content)
+
 
 pdf.output("output.pdf")
